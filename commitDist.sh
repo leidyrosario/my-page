18c8edd9b95d8ae813f49b9ee5c6b4c203c1d9a3
Giancarlo Trezzi <giancarlotrezzi@gmail.com>
2020-04-18 18:00:56 +0200

    new version

---
+++ commitDist.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+rm -fR /Users/Gianca/Google\\ Drive/www/leidyDist/*
+cp -R dist/leidy/* /Users/Gianca/Google\\ Drive/www/leidyDist
+cd /Users/Gianca/Google\\ Drive/www/leidyDist/
+git add .
+git commit -m "new version"
+git push -u origin master
+cd /Users/Gianca/Google\ Drive/GitHub/lady
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes, {
       scrollPositionRestoration: 'enabled',
       anchorScrolling: 'enabled',
-      useHash: false
+      useHash: true
     })
   ],
   exports: [RouterModule]
