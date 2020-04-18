b7e010cadbb98c6a7d1bd559a29182caf8ce4673
Giancarlo Trezzi <giancarlotrezzi@gmail.com>
2020-04-18 18:11:34 +0200

    inserito bash per pubblicazione veloce + fix al router hash

--- commitDist.sh
+++ commitDist.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-rm -fR /Users/Gianca/Google\\ Drive/www/leidyDist/*
-cp -R dist/leidy/* /Users/Gianca/Google\\ Drive/www/leidyDist
-cd /Users/Gianca/Google\\ Drive/www/leidyDist/
+rm -fR /Users/Gianca/Google\ Drive/www/leidyDist/*
+cp -R dist/leidy/* /Users/Gianca/Google\ Drive/www/leidyDist
+cd /Users/Gianca/Google\ Drive/www/leidyDist/
 git add .
 git commit -m "new version"
 git push -u origin master
