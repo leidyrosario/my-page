aa35e3e890510d77c5b10ea94ca95242e1809e65
Leidy Rosario <ledy_93@hotmail.it>
2020-04-24 20:41:09 +0200

    modifiche

--- commitDistLeidy.sh
+++ commitDistLeidy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-rm -fR /Users/lady_rosario/Documenti/leidyDist/*
-cp -R dist/leidy/* /Users/lady_rosario/Documenti/leidyDist
-cd /Users/lady_rosario/Documenti/leidyDist/
+rm -fR /Users/lady_rosario/Documents/leidyDist/leidyrosario.github.io/*
+cp -R dist/leidy/* /Users/lady_rosario/Documents/leidyDist/leidyrosario.github.io
+cd /Users/lady_rosario/Documents/leidyDist/leidyrosario.github.io
 git add .
 git commit -m "new version"
 git push -u origin master
